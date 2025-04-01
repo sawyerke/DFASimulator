d914bcfc210f1434ac08e01bdb8177872064116e
sawyerke <sawyerke1211@gmail.com>
2025-04-01 11:29:38 -0500

    Created main. General input

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+statesStr = input("\n Enter the set of states separated by \",\": \n")
+startState = input("\n Which state is the starting state? \n")
+accept = input("\n Enter the set of accepting states separated by \",\": \n")
+languageStr = input("\n Enter the set of input symbols separated by \",\": \n")
+
+language = languageStr.split(', ')
+states = statesStr.split(', ')
+acceptingStates = accept.split(', ')
+
+transitions = []
+
+
+print("\t", end="")
+for symbol in language:
+    print(symbol + "\t", end="")
+print()
+
+for state in states:
+    print(state, end="")
+    y = input("\t")
+    x = y.split('\t')
+    transitions.append(x)
+
+
+
+
+
+print(transitions)
+
+    
